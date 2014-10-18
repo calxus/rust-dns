730d3aa4163f848d915baae6565e1bad4e478279
gtadam <gtadam89@gmail.com>
2014-10-18 18:59:42 +0100

    Server now accepts udp connections and reads stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(slicing_syntax)]
+
+use std::io::net::udp::UdpSocket;
+use std::io::net::ip::{Ipv4Addr, SocketAddr};
 fn main() {
-    println!("Hello, world!")
+    let addr = SocketAddr { ip: Ipv4Addr(127, 0, 0, 1), port: 53 };
+    let mut socket = match UdpSocket::bind(addr) {
+        Ok(s) => s,
+        Err(e) => fail!("couldn't bind socket: {}", e),
+    };
+
+    let mut buf = [0, ..1024];
+    match socket.recv_from(buf) {
+        Ok(T) => {println!("{}", T.to_string())},
+        Err(e) => println!("couldn't receive a datagram: {}", e)
+    }
+    drop(socket); // close the socket
 }
